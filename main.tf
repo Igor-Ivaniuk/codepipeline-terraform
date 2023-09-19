fee5431a1eddd0ea8f467258f33eca1c35b3a503
Igor Ivaniuk <iivaniuk@amazon.com>
2023-09-19 20:20:49 +0200

    Added costs variables

--- main.tf
+++ main.tf
@@ -16,29 +16,29 @@ locals {
   region     = data.aws_region.current.name
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.amazon-linux-2.id
-  instance_type = "t3.micro"
+# resource "aws_instance" "web" {
+#   ami           = data.aws_ami.amazon-linux-2.id
+#   instance_type = "t3.micro"
 
-  tags = {
-    Name = "Terraform-ExampleServer"
-  }
-}
+#   tags = {
+#     Name = "Terraform-ExampleServer"
+#   }
+# }
 
-resource "aws_s3_bucket" "mybucket" {
-  bucket = "iivaniuk.createdbytf"
-}
+# resource "aws_s3_bucket" "mybucket" {
+#   bucket = "iivaniuk.createdbytf"
+# }
 
-resource "aws_s3_bucket_ownership_controls" "mybucket_owncontrol" {
-  bucket = aws_s3_bucket.mybucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
+# resource "aws_s3_bucket_ownership_controls" "mybucket_owncontrol" {
+#   bucket = aws_s3_bucket.mybucket.id
+#   rule {
+#     object_ownership = "BucketOwnerPreferred"
+#   }
+# }
 
-resource "aws_s3_bucket_acl" "private" {
-  depends_on = [aws_s3_bucket_ownership_controls.mybucket_owncontrol]
+# resource "aws_s3_bucket_acl" "private" {
+#   depends_on = [aws_s3_bucket_ownership_controls.mybucket_owncontrol]
 
-  bucket = aws_s3_bucket.mybucket.id
-  acl    = "private"
-}
+#   bucket = aws_s3_bucket.mybucket.id
+#   acl    = "private"
+# }
