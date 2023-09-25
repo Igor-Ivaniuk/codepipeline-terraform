42d6745ff346011eed69b3fa5a952580b15ef46c
Igor Ivaniuk <iivaniuk@amazon.com>
2023-09-26 01:24:25 +0200

    Removed 2nd NG

--- tf/eks.tf
+++ tf/eks.tf
@@ -68,14 +68,14 @@ module "eks" {
       desired_size = 2
     }
 
-    two = {
-      name = "node-group-2"
+    # two = {
+    #   name = "node-group-2"
 
-      instance_types = ["t3.small"]
+    #   instance_types = ["t3.small"]
 
-      min_size     = 1
-      max_size     = 10
-      desired_size = 2
-    }
+    #   min_size     = 1
+    #   max_size     = 10
+    #   desired_size = 2
+    # }
   }
 }
