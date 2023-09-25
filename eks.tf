91c34375bf1539ddb61131be342c7217e86b48cd
Igor Ivaniuk <iivaniuk@amazon.com>
2023-09-25 23:03:20 +0200

    Changed usage of EKS

--- eks.tf
+++ eks.tf
@@ -67,15 +67,5 @@ module "eks" {
       max_size     = 3
       desired_size = 2
     }
-
-    two = {
-      name = "node-group-2"
-
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-    }
   }
 }
