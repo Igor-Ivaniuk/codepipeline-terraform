bc682b7fab29d9bffddff62c1906608e55699481
Igor Ivaniuk <iivaniuk@amazon.com>
2023-11-29 08:56:00 -0800

    Small instance

--- tf/main.tf
+++ tf/main.tf
@@ -34,13 +34,13 @@ resource "aws_instance" "t3_large" {
   }
 }
 
-# resource "aws_instance" "t3_nano" {
-#   ami           = data.aws_ami.amazon-linux-2.id
-#   instance_type = "t3.nano"
-#   tags = {
-#     Name = "Server-t3.nano"
-#   }
-# }
+resource "aws_instance" "t3_nano" {
+  ami           = data.aws_ami.amazon-linux-2.id
+  instance_type = "t3.nano"
+  tags = {
+    Name = "Server-t3.nano"
+  }
+}
 
 # resource "aws_instance" "t3_2xlarge" {
 #   ami           = data.aws_ami.amazon-linux-2.id
