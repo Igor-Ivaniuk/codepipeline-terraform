b4f55bbb299c2ff2634feb6ec14c4da94dd56768
Igor Ivaniuk <iivaniuk@amazon.com>
2023-11-28 18:01:39 -0800

    Deploying larger EC2

--- tf/main.tf
+++ tf/main.tf
@@ -42,10 +42,10 @@ resource "aws_instance" "t3_nano" {
   }
 }
 
-# resource "aws_instance" "t3_2xlarge" {
-#   ami           = data.aws_ami.amazon-linux-2.id
-#   instance_type = "t3.2xlarge"
-#   tags = {
-#     Name = "Server-t3.2xlarge"
-#   }
-# }
+resource "aws_instance" "t3_2xlarge" {
+  ami           = data.aws_ami.amazon-linux-2.id
+  instance_type = "t3.2xlarge"
+  tags = {
+    Name = "Server-t3.2xlarge"
+  }
+}
